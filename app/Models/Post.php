b99b0e1a2a8993732a6b90318c279b4fa41c3755
Yasin Korkmaz <theacherx@gmail.com>
2023-10-14 15:08:14 +0300

    Creating N+1 Issue

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -18,7 +18,7 @@ class Post extends Model
 
     public function author()
     {
-        return $this->belongsTo(Auth::class);
+        return $this->belongsTo(Author::class);
     }
 
     public function category()
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,7 @@
 @foreach($posts as $post)
     {{$post->title}} <br>
     {{$post->content}}<br>
+    {{$post->author->name}}<br>
+    {{$post->category->name}}<br>
     <hr>
 @endforeach
