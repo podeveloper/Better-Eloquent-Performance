34796d7d80f3024e832fbd6849debf89c710b4e5
Yasin Korkmaz <theacherx@gmail.com>
2023-10-14 18:22:11 +0300

    Selecting Columns in With Function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::with(['author:id,name','category:id,name'])->get();
         return view('posts.index',compact('posts'));
     }
 
